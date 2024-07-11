3e524a409861334116d6efb67f7107aa08e39670
Juan.Pinol <juan.pinol@cvent.com>
2024-07-11 17:47:44 -0400

    Did most of the number functionalities.

--- src/main/java/com/OOP/ObjectOrientedCarDealership/repositories/VehicleRepository.java
+++ src/main/java/com/OOP/ObjectOrientedCarDealership/repositories/VehicleRepository.java
@@ -2,6 +2,7 @@ package com.OOP.ObjectOrientedCarDealership.repositories;
 
 import com.OOP.ObjectOrientedCarDealership.models.Vehicle;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
@@ -15,12 +16,13 @@ import java.util.logging.Logger;
 import java.util.logging.Level;
 
 @Repository
+@Component
 public class VehicleRepository {
 
     private static final Logger logger = Logger.getLogger(VehicleRepository.class.getName());
 
     @Autowired
-    private DataSource dataSource;
+    public DataSource dataSource;
 
     public List<Vehicle> getAllVehicles() {
         String query = """
@@ -59,6 +61,223 @@ public class VehicleRepository {
         }
 
         return vehicles;
+    }
+
+    public List<Vehicle> getVehiclesByPrice(float min, float max) {
+        List<Vehicle> vehicles = new ArrayList<>();
+        String query = """
+        SELECT
+                v.vehicle_id,
+                v."VIN",
+                v.sold,
+                v.color,
+                v.electric,
+                v.miles,
+                v.doors,
+                v.convertible,
+                v.make,
+                v.model,
+                v.year,
+                v.price,
+                vt.type AS vehicle_type
+        FROM
+        vehicles v
+        JOIN
+        vehicle_types vt ON v.vehicle_type = vt.type_id
+        WHERE v.price > ? AND v.price < ?;""";
+
+        try(Connection conn = dataSource.getConnection();
+                PreparedStatement ps = conn.prepareStatement(query)
+        ) {
+            ps.setFloat(1, min);
+            ps.setFloat(2, max);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                vehicles.add(mapToVehicle(rs));
+            }
+
+        } catch (SQLException e) {
+            logger.log(Level.SEVERE, "An error occurred: " + e.getMessage(), e);
+        }
+        return vehicles;
+    }
+
+    public List<Vehicle> getVehicleByMakeModel(String make, String model) {
+        List<Vehicle> vehicles = new ArrayList<>();
+        String query = """
+        SELECT
+                v.vehicle_id,
+                v."VIN",
+                v.sold,
+                v.color,
+                v.electric,
+                v.miles,
+                v.doors,
+                v.convertible,
+                v.make,
+                v.model,
+                v.year,
+                v.price,
+                vt.type AS vehicle_type
+        FROM
+        vehicles v
+        JOIN
+        vehicle_types vt ON v.vehicle_type = vt.type_id
+        WHERE UPPER(v.make) = UPPER(?) AND UPPER(v.model) = UPPER(?);""";
+
+        try(Connection conn = dataSource.getConnection();
+                PreparedStatement ps = conn.prepareStatement(query)
+        ) {
+            ps.setString(1, make);
+            ps.setString(2, model);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                vehicles.add(mapToVehicle(rs));
+            }
+
+        } catch (SQLException e) {
+            logger.log(Level.SEVERE, "An error occurred: " + e.getMessage(), e);
+        }
+        return vehicles;
+    }
+
+    public List<Vehicle> getVehiclesByYearRange(int lowestYear, int highestYear) {
+        List<Vehicle> vehicles = new ArrayList<>();
+        String query = """
+        SELECT
+                v.vehicle_id,
+                v."VIN",
+                v.sold,
+                v.color,
+                v.electric,
+                v.miles,
+                v.doors,
+                v.convertible,
+                v.make,
+                v.model,
+                v.year,
+                v.price,
+                vt.type AS vehicle_type
+        FROM
+        vehicles v
+        JOIN
+        vehicle_types vt ON v.vehicle_type = vt.type_id
+        WHERE v.year >= ? AND v.year <= ?;""";
+
+        try(Connection conn = dataSource.getConnection();
+                PreparedStatement ps = conn.prepareStatement(query)
+        ) {
+            ps.setInt(1, lowestYear);
+            ps.setInt(2, highestYear);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                vehicles.add(mapToVehicle(rs));
+            }
+
+        } catch (SQLException e) {
+            logger.log(Level.SEVERE, "An error occurred: " + e.getMessage(), e);
+        }
+        return vehicles;
+    }
+
+    public List<Vehicle> getVehiclesByColor(String color) {
+        List<Vehicle> vehicles = new ArrayList<>();
+        String query = """
+        SELECT
+                v.vehicle_id,
+                v."VIN",
+                v.sold,
+                v.color,
+                v.electric,
+                v.miles,
+                v.doors,
+                v.convertible,
+                v.make,
+                v.model,
+                v.year,
+                v.price,
+                vt.type AS vehicle_type
+        FROM
+        vehicles v
+        JOIN
+        vehicle_types vt ON v.vehicle_type = vt.type_id
+        WHERE UPPER(v.color) = UPPER(?);""";
+
+        try(Connection conn = dataSource.getConnection();
+                PreparedStatement ps = conn.prepareStatement(query)
+        ) {
+            ps.setString(1, color);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                vehicles.add(mapToVehicle(rs));
+            }
+
+        } catch (SQLException e) {
+            logger.log(Level.SEVERE, "An error occurred: " + e.getMessage(), e);
+        }
+        return vehicles;
+    }
+
+    public List<Vehicle> getVehiclesByMileRange(int lowestYear, int highestYear) {
+        List<Vehicle> vehicles = new ArrayList<>();
+        String query = """
+        SELECT
+                v.vehicle_id,
+                v."VIN",
+                v.sold,
+                v.color,
+                v.electric,
+                v.miles,
+                v.doors,
+                v.convertible,
+                v.make,
+                v.model,
+                v.year,
+                v.price,
+                vt.type AS vehicle_type
+        FROM
+        vehicles v
+        JOIN
+        vehicle_types vt ON v.vehicle_type = vt.type_id
+        WHERE v.miles >= ? AND v.miles <= ?;""";
+
+        try(Connection conn = dataSource.getConnection();
+                PreparedStatement ps = conn.prepareStatement(query)
+        ) {
+            ps.setInt(1, lowestYear);
+            ps.setInt(2, highestYear);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                vehicles.add(mapToVehicle(rs));
+            }
+
+        } catch (SQLException e) {
+            logger.log(Level.SEVERE, "An error occurred: " + e.getMessage(), e);
+        }
+        return vehicles;
+    }
+
+    public Vehicle removeVehicleById(int id) {
+        Vehicle vehicle = new Vehicle();
+        String query = """
+        DELETE
+        FROM
+        vehicles
+        WHERE vehicle_id = ?;""";
+
+        try(Connection conn = dataSource.getConnection();
+                PreparedStatement ps = conn.prepareStatement(query)
+        ) {
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                vehicle = mapToVehicle(rs);
+            }
+
+        } catch (SQLException e) {
+            logger.log(Level.SEVERE, "An error occurred: " + e.getMessage(), e);
+        }
+        return vehicle;
     }
 
     Vehicle mapToVehicle(ResultSet rs) {
